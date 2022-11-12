404369c09e948a9e6028c16e653060ec0eeccae1
Evandro Maddes <e.maddes@reply.it>
2022-11-12 17:32:30 +0100

    password encoder

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -5,8 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.websocket.server.PathParam;
-import java.util.ArrayList;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,16 +13,17 @@ import java.util.List;
 @RequestMapping("/api/v1/students")
 public class StudentController {
     private final List<Student> STUDENT = Arrays.asList(
-            new Student(1 , "Evandro"),
-            new Student(2 , "Angela"),
-            new Student(3 , "Emanuele")
+            new Student(1, "Evandro"),
+            new Student(2, "Angela"),
+            new Student(3, "Emanuele")
     );
-    @GetMapping(path = "{studentId}")
-    public Student getStudent(@PathVariable("studentId") Integer studentId){
 
-    return STUDENT.stream()
-            .filter(student -> student.getStudentId().equals(studentId))
-            .findFirst()
-            .orElseThrow(() -> new IllegalStateException("No user found with ID: " + studentId));
+    @GetMapping(path = "{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId) {
+
+        return STUDENT.stream()
+                .filter(student -> student.getStudentId().equals(studentId))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("No user found with ID: " + studentId));
     }
 }
